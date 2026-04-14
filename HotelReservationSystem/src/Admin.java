0b3a8d12834d3c163c9f2243ea6c63119dd395ba
omarelgharably <omar.elgharably@gmail.com>
2026-04-14 13:26:19 +0200

    Room and RoomType methods is done in admin

--- HotelReservationSystem/src/Admin.java
+++ HotelReservationSystem/src/Admin.java
@@ -10,13 +10,9 @@ public class Admin extends Staff{
         super.setWorkingHours(hours);
     }
 
-
+    // Rooms Methods
     public void viewRooms(){
         Room.read();
-    }
-
-       public void viewRoomTypes(){
-        RoomType.read();
     }
 
     public void addRoom(int roomNo, int floor, Room.view view, RoomType type, ArrayList<Amenity> amenities) throws InvalidInputException {
@@ -25,11 +21,65 @@ public class Admin extends Staff{
             Room.create(r);
         }
         catch (Exception e){
-            throw new InvalidInputException("Can't Add This Room");
+            throw e;
         }
     }
 
-    public void removeRoom(int roomNo){
+    public void removeRoom(int index) throws InvalidInputException{
+        try{
+            HotelDataBase.rooms.get(index).delete(index);
+        }
+        catch (Exception e){
+            throw e;
+        }
+
+    }
+
+    public void updateRoom(int roomNo, int floor, Room.view view, RoomType type, ArrayList<Amenity> amenities, int index) throws InvalidInputException{
+        Room r = new Room(type, amenities,roomNo,floor,view);
+        try {
+            HotelDataBase.rooms.get(index).update(r);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+
+
+    //RoomTypes Methods
+
+    public void viewRoomTypes(){
+        RoomType.read();
+    }
+
+    public void addRoomType(String size, double basePrice, int capacity) throws InvalidInputException{
+        RoomType rt = new RoomType(size, basePrice, capacity);
+        try {
+            RoomType.create(rt);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+
+    public void removeRoomType(int index) throws InvalidInputException{
+        try{
+            HotelDataBase.roomTypes.get(index).delete(index);
+        }
+        catch (Exception e){
+            throw e;
+        }
+
+    }
+
+    public void updateRoomTypes(String size , double basePrice, int capacity, int index) throws InvalidInputException{
+         RoomType rt = new RoomType(size, basePrice,capacity);
+         try{
+             HotelDataBase.roomTypes.get(index).update(rt);
+         }
+         catch(Exception e){
+             throw e;
+         }
 
     }
 
